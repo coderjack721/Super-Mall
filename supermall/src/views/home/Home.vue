6f1db004ad4b0f86660bcde62b203b9ebfd934e7
coderjack721 <1120128678@qq.com>
2021-08-06 23:57:10 +0800

    q

--- supermall/src/views/home/Home.vue
+++ supermall/src/views/home/Home.vue
@@ -1,22 +1,133 @@
 <template>
   <div id="home">
     <NavBar class="home-nav"><div slot="center">购物街</div></NavBar>
+    <HomeSwiper :banners="banners"/>
+    <RecommendView :recommends="recommends"/>
+    <FeatureView/>
+    <TabControl class="tab_control" :titles="['流行','新款','精选']"/>
+    <ul>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
+
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
+
+
+import { getHomeMultidata } from "network/home";
+
+
 export default {
     name:"Home",
     components:{
-      NavBar
+      NavBar,
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+      TabControl
+    },
+    data(){
+      return{
+        recommends:[],
+        banners:[]
+      }
+    },
+    created(){
+      getHomeMultidata().then(res => {
+        console.log(res);
+        this.recommends = res.data.recommend.list;
+        this.banners = res.data.banner.list;
+      })
     }
 }
 </script>
 
 <style>
+#home{
+  padding-top: 44px;
+}
 .home-nav{
   background-color: var(--color-tint);
   color: #fff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
+.tab-control{
+  position: sticky;
+  top: 44px;
 }
 </style>
---
+++ supermall/src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="recommend">
+        <div v-for="(item,aaa) in recommends" :key="aaa" class="recommend-item">
+            <a :href="item.link">
+            <img :src="item.image" alt="">
+            <div>{{item.title}}</div>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"RecommendView",
+    props:{
+        recommends:{
+            type:Array,
+            default(){
+                return []
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.recommend{
+    display: flex;
+    text-align: center;
+    font-size: 14px;
+    padding: 10px 0 18px;
+    border-bottom: 10px solid #eee;;
+}
+.recommend-item{
+    flex: 1;
+}
+.recommend-item img {
+    width: 70px;
+    height: 70px;
+    margin-bottom: 10px;
+}
+</style>
